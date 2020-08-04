2552ecbe53e23c2210ee9c023b925e2bde91494b
Farooq Mahmud <farooq@teqniqly.com>
2020-08-04 12:30:52 -0700

    wip

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
     shape_files_root = os.path.join(data_root, "shp")
     shape_file_collection = "cb_2018_us_state_500k"
 
-    geo_df = gp.read_file(os.path.join(shape_files_root, shape_file_collection, f"{shape_file_collection}.shp"))
+    geo_df = gp.read_file(
+        os.path.join(
+            shape_files_root, shape_file_collection, f"{shape_file_collection}.shp"
+        )
+    )
     geo_df = geo_df.to_crs(epsg=3857)
 
     _, ax = plt.subplots(1, figsize=(60, 20))
